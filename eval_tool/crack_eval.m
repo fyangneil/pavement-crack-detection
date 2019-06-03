f3583543e0ce273164d7ea71bd678422023f48ea
fyangneil <46222158+fyangneil@users.noreply.github.com>
2019-06-03 03:47:19 -0700

    Update crack_eval.m

--- eval_tool/crack_eval.m
+++ eval_tool/crack_eval.m
@@ -1,7 +1,7 @@
 model='FPHB';
 fprintf('%s\n',model);
-resDir='/media/fan/dv2/backup/fan_part3/home/fy/fan/hed/data/crack/testresults/tmp/nms';
-gtDir='/media/fan/dv2/backup/fan_part3/home/fy/fan/hed/data/crack/testcrop';
+resDir='/data/crack/testresults/tmp/nms';
+gtDir='/data/crack/testcrop';
 edgesEvalDir_crack('resDir',resDir,'gtDir',gtDir, 'thin', 1, 'pDistr',{{'type','parfor'}},'maxDist',0.0075);
 figure; edgesEvalPlot(resDir,model);
 close all
